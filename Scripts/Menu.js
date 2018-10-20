c6bfd3aabeddbbf5f15a3e4a2e2a9d70851b0b56
MeganKSpence <43014740+MeganKSpence@users.noreply.github.com>
2018-10-20 15:24:02 -0600

    update

---
+++ Scripts/Menu.js
@@ -0,0 +1,1 @@
+console.log("Welcome to the Menu.js of Fries and Friends!");
---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Welcome to the index.js of Fries and Friends!");
+console.log("No JavaScript was required to make this page function");
